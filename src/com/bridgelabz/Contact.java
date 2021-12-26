cdb971dadb5e2033dcf56bf57f8c74025d42ef4d
akashbg3010 <akashbpura@gmail.com>
2021-12-26 15:56:00 +0530

    Modified few lines in contact

--- src/com/bridgelabz/Contact.java
+++ src/com/bridgelabz/Contact.java
@@ -3,21 +3,20 @@ package com.bridgelabz;
 public class Contact {
 	
 	String firstName,lastName,address,state,email;
-	int zip;
+	int pin;
 	long phoneNumber;
 	Contact(String firstName,String lastName,String address,String state,int zip,long phoneNumber,String email){
 		this.firstName=firstName;
 		this.lastName=lastName;
 		this.address=address;
 		this.state=state;
-		this.zip=zip;
+		this.pin=pin;
 		this.phoneNumber=phoneNumber;
+		this.email=email;
 	}
 	@Override
 	public String toString() {
 		return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", state="
-				+ state + ", email=" + email + ", zip=" + zip + ", phoneNumber=" + phoneNumber + "]";
+				+ state + ", email=" + email + ", pin=" + pin + ", phoneNumber=" + phoneNumber + "]";
 	}
-	
-
 }
